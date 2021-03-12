59a130c436b779d6e224b2b2066b35c307d475ad
Chris Kamp <chriskamp101@gmail.com>
2021-03-12 18:07:05 +1000

    add mobile nav transition effects

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,31 @@
 const toggle = document.getElementById("m-nav-toggle");
 const nav = document.getElementById("m-nav");
+
+// ensure styles are set to initial values on page load
 nav.style.display = "none";
-toggle.style.bottom = "30px";
+nav.style.bottom = "-60vw";
+nav.style.transform = "scale(0.1)";
+toggle.style.bottom = "5vh";
 
 toggle.addEventListener("click", () => {
   if (nav.style.display === "none") {
     nav.style.display = "flex";
-    toggle.style.bottom = "55vw";
+
+    // forces re-draw to allow opacity transition (otherwise prevented by display: none)
+    const height = nav.offsetHeight;
+    nav.style.bottom = "5vw";
+    nav.style.transform = "scale(1)";
+    toggle.style.bottom = "58vw";
+    toggle.style.right = "5vw";
   } else {
-    nav.style.display = "none";
-    toggle.style.bottom = "30px";
+    nav.style.bottom = "-60vw";
+    nav.style.transform = "scale(0.1)";
+    toggle.style.bottom = "5vh";
+    toggle.style.right = "10vw";
+
+    // allow time for transitions to complete
+    setTimeout(() => {
+      nav.style.display = "none";
+    }, 300);
   }
 });
